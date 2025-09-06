d8d5a47561d90ba5ddc0a7db2a5fb172a00c0652
osaeid rony <osaeidfire@gmail.com>
2025-09-06 17:20:35 +0100

    pyhton

---
+++ main.py
@@ -0,0 +1,17 @@
+def Sum(num1, num2):
+    result = num1 + num2
+    return result
+
+def double(num1):
+    x = num1*2
+    return x
+
+result = Sum(5,8)
+x = double(6)
+
+print(result)
+
+print(x)
+
+def Names():
+    
